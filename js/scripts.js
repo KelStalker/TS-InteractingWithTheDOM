ce9fbd0275b445a655a850500be1eb29b40032d8
Kel <kelstalker@hotmail.com>
2020-04-06 09:32:09 -0600

    All pages built, no errors, but input not populating in DOM list

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var myform = document.querySelector("form");
+var myInput = document.querySelector('[name="list=item"]');
+var myList = document.querySelector("ul");
+myform.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var formValue = myInput.value;
+    var newListItem = document.createElement('LI');
+    newListItem.textContent = formValue; //populate the element
+    myList.appendChild(newListItem);
+});
---
+++ src/ts/main.ts
@@ -0,0 +1,13 @@
+const myform: HTMLElement = document.querySelector("form");
+const myInput: HTMLInputElement = document.querySelector('[name="list=item"]');
+const myList: HTMLElement = document.querySelector("ul");
+
+myform.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formValue: string = myInput.value;
+    const newListItem: HTMLElement = document.createElement( 'LI');
+    newListItem.textContent = formValue; //populate the element
+
+    myList.appendChild( newListItem );
+});
